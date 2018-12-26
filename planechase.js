584f8eabbac753a203efeb9dde8a114535f93384
Ian Jones <ianjones@stanford.edu>
2018-12-25 18:07:01 -0800

    lots of changes... all fadein/out animations work, starting to work on player names for life totals and phenomena stuff

--- planechase.js
+++ planechase.js
@@ -1,15 +1,26 @@
+/*
+ * planechase.js
+ *************************
+ * Handles logic and animations for Planechase
+ * 
+ */
 
-const NUM_PLANES = 86;
+
+const NUM_PLANES = 85;
 const NUM_ROWS = 3;
 const NUM_COLS = 3;
-const TRANSITION_DURATION = 1000;
+const TRANSITION_DURATION = 1000;               // All animations
+const LOADING_DURATION = 2500;                  // Initial loading gif duration
+const PHENOMENA = [2, 5, 6, 7, 8, 9, 17];       // Keep track of phenomena image indices
+const SPATIAL_MERGING = 6;                      // Keep track of Spatial Merging image index
+
 var deck = [];
-// curr plane will be index 4 of the grid, always recenter
-var grid = [
+var grid = [            // Current plane will be index 4 of the grid, always recenter
     [-1, -1, -1],
     [-1, -1, -1],
     [-1, -1, -1]
 ];
+var currFocus;          // Current zoomed in card
 
 
 
@@ -17,6 +28,9 @@ var grid = [
  * Highlight cell
  */
 function on_mouseover(cell) {
+    // Remove other animations
+    cell.classList.remove("animate-fadeIn");
+    cell.classList.remove("animate-fadeOut");
     cell.style.opacity = 0.5;
 }
 
@@ -35,28 +49,42 @@ function on_mouseout(cell) {
 function focusPlane(indices) {
     var row = indices[0];
     var col = indices[1];
+    currFocus = indices;
 
-    // Plane is face-up
-    if(grid[row][col] != -1) {
-        var background = document.querySelector(".focusPlaneBackground");
-        var focusPlane = document.querySelector(".focusPlane");
+    var background = document.querySelector(".focusPlaneBackground");
+    var focusPlane = document.querySelector(".focusPlane");
+    var planeswalkButton = document.querySelector(".planeswalkButton");
 
-        background.style.display = "inline";
-        focusPlane.style.display = "flex"; 
+    background.style.display = "inline";
+    focusPlane.style.display = "flex"; 
 
-        var imageName = "assets/planes/" + grid[row][col] + ".jpg";
-        focusPlane.getElementsByTagName("img")[0].src = imageName; 
+    // Don't give planeswalk option if looking at current plane
+    if(!(row == 1 && col == 1)) {
+        planeswalkButton.style.display = "flex";
+    }
 
-        // Animate fadeIns of clickable background and plane image
-        background.classList.remove("animate-fadeOut");
-        background.classList.remove("animate-fadeIn");
-
-        focusPlane.classList.remove("animate-fadeOut");
-        focusPlane.classList.add("animate-fadeIn");
-
+    // Set image for focus
+    var imageName;
+    // Plane is face-up 
+    if (grid[row][col] != -1) {
+        imageName = "assets/planes/" + grid[row][col] + ".png";
     // Plane is face-down
     } else {
-        console.log("not face-up")
+        imageName = "assets/planes/back.png";
+    }
+    focusPlane.getElementsByTagName("img")[0].src = imageName; 
+
+    // Animate fadeIns of clickable background, plane image, and planeswalk button
+    background.classList.remove("animate-fadeOut");
+    background.classList.add("animate-fadeIn");
+
+    focusPlane.classList.remove("animate-fadeOut");
+    focusPlane.classList.add("animate-fadeIn");
+
+    // Don't give planeswalk option if looking at current plane
+    if(!(row == 1 && col == 1)) {
+        planeswalkButton.classList.remove("animate-fadeOut");
+        planeswalkButton.classList.add("animate-fadeIn"); 
     }
 }
 
@@ -67,17 +95,22 @@ function focusPlane(indices) {
 function removeFocusPlane() {
     var background = document.querySelector(".focusPlaneBackground");
     var focusPlane = document.querySelector(".focusPlane");
+    var planeswalkButton = document.querySelector(".planeswalkButton");
 
-    // Animates fadeOuts of clickable background and plane image
+    // Animates fadeOuts of clickable background, plane image, and planeswalk button
     background.classList.remove("animate-fadeIn");
     background.classList.add("animate-fadeOut");
 
     focusPlane.classList.remove("animate-fadeIn");
     focusPlane.classList.add("animate-fadeOut");
 
+    planeswalkButton.classList.remove("animate-fadeIn");
+    planeswalkButton.classList.add("animate-fadeOut"); 
+
     setTimeout(function() {
         background.style.display = "none";
         focusPlane.style.display = "none";
+        planeswalkButton.style.display = "none";
     }, TRANSITION_DURATION);
 }
 
@@ -85,58 +118,73 @@ function removeFocusPlane() {
 /*
  * Gets new cards from the deck and shifts the grid to keep centered
  */
-function moveToPlane(indices) {
+function moveToPlane() {
 
-    var rowShift = 1 - indices[0];
-    var colShift = 1 - indices[1];
+    // Can't walk to the current plane
+    if (currFocus[0] == 1 && currFocus[1] == 1) return;
 
-    // Keep track of new grid
-    var temp = [
-        [-1, -1, -1],
-        [-1, -1, -1],
-        [-1, -1, -1]
-    ]; 
+    removeFocusPlane();
+    fadeOutCards();
 
-    // Iterate through grid, shifting the planes
-    for (var i = 0; i < NUM_ROWS; i++) {
-        for (var j = 0; j < NUM_COLS; j++) {
+    // Wait till removeFocusPlane() is done
+    setTimeout(function() {
 
-            // Only shift if in bounds
-            if (i - rowShift >= 0 && i - rowShift < NUM_ROWS &&
-                j - colShift >= 0 && j - colShift < NUM_COLS) {
+        var rowShift = 1 - currFocus[0];
+        var colShift = 1 - currFocus[1];
 
-                temp[i][j] = grid[i - rowShift][j - colShift];
+        // Keep track of new grid
+        var temp = [
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1]
+        ]; 
+
+        // Iterate through grid, shifting the planes
+        for (var i = 0; i < NUM_ROWS; i++) {
+            for (var j = 0; j < NUM_COLS; j++) {
+
+                // Only shift if in bounds
+                if (i - rowShift >= 0 && i - rowShift < NUM_ROWS &&
+                    j - colShift >= 0 && j - colShift < NUM_COLS) {
+
+                    temp[i][j] = grid[i - rowShift][j - colShift];
+                }
             }
         }
-    }
 
-    // Get new cards from deck
-    if (temp[0][1] == -1){
-        temp[0][1] = deck.shift();
-    }
-    if (temp[1][0] == -1){
-        temp[1][0] = deck.shift();
-    }
-    if (temp[1][1] == -1){
-        temp[1][1] = deck.shift();
-    }
-    if (temp[1][2] == -1){
-        temp[1][2] = deck.shift();
-    }
-    if (temp[2][1] == -1){
-        temp[2][1] = deck.shift();
-    }
-    
-    // redraw board    
-    grid = temp;
-    renderBoard();
+        // Get new cards from deck
+        if (temp[0][1] == -1){
+            temp[0][1] = drawCard();
+        }
+        if (temp[1][0] == -1){
+            temp[1][0] = drawCard();
+        }
+        if (temp[1][1] == -1){
+            temp[1][1] = drawCard(); 
+        }
+        if (temp[1][2] == -1){
+            temp[1][2] = drawCard(); 
+        }
+        if (temp[2][1] == -1){
+            temp[2][1] = drawCard(); 
+        }
+        
+        // redraw board    
+        grid = temp;
+        renderBoard(false);
+    }, TRANSITION_DURATION);
 }
 
 
 /*
  * Draws the board according to the grid
+ * @Params: 
+ * wait -- true if rendering should wait for images to load longer (initial game setup)
  */
-function renderBoard() {
+function renderBoard(wait) {
+    var duration = wait ? LOADING_DURATION : 50;
+    var startBackground = document.querySelector(".startBackground");
+    startBackground.style.display = "inline"; 
 
     // Iterate through all grid cells, draw card face-up/face-down depending on value
     for (var i = 0; i < NUM_ROWS; i++) {
@@ -145,23 +193,124 @@ function renderBoard() {
             // Draw plane
             if (grid[i][j] != -1) {
                 var cell = document.getElementById("cell" + i + j).getElementsByTagName("img")[0]; 
-                var imageName = "assets/planes/" + grid[i][j] + ".jpg";
+                var imageName = "assets/planes/" + grid[i][j] + ".png";
                 cell.src = imageName;
 
             // No plane in this cell, draw placeholder
             } else {
                 var cell = document.getElementById("cell" + i + j).getElementsByTagName("img")[0]; 
-                cell.src = "assets/planes/back.jpg";
+                cell.src = "assets/planes/back.png";
             }
         }
     }
+
+    var cells = document.querySelectorAll(".cell");
+    var loadingGif = document.querySelector(".loading-gif");
+
+    // Wait for images to load, then fade them in and remove loading gif
+    setTimeout(function() {
+        loadingGif.classList.remove("animate-fadeIn");
+        loadingGif.classList.add("animate-fadeOut");
+
+        cells.forEach(function(d) {
+            d.classList.remove("animate-fadeOut");
+            d.classList.add("animate-fadeIn");
+        }) 
+
+        loadingGif.classList.remove("animate-fadeOut");
+        loadingGif.style.display = "none";
+
+        startBackground.style.display = "none";
+    }, duration);
 }
 
 
 /*
- * Sets up the game with the 5 starting planes in a cross
+ * Animates fadeOut of the start button and then starts the game
  */
 function setUpGame() {
+    var startBackground = document.querySelector(".startBackground");
+    var startButton = document.querySelector(".startButton");
+    var restartButtonTemp = document.querySelector(".restartButtonTemp");
+    var restartButton = document.querySelector(".restartButton");
+
+    // Get rid of hover animations
+    startButton.onmouseout = null;
+    startButton.onmouseover = null;
+
+    // Remove placeholder
+    restartButtonTemp.style.display = "none";
+    restartButton.style.display = "flex";
+
+    startButton.classList.add("animate-fadeOut");
+    restartButton.classList.add("animate-fadeIn");
+
+    // Wait for buttons to disappear before prompting for player names
+    setTimeout(function() {
+        startButton.style.display = "none";
+
+        // Get player names before rendering the board
+        // setupLifeTotals(function() {
+        //     console.log("done with setting up lifetotals");
+        //     dealCards()
+        // });
+
+        dealCards();
+    }, TRANSITION_DURATION);
+}
+
+
+/*
+ *
+ */
+function setupLifeTotals() {
+
+    var submitted = false;
+
+    // Listen for if user has input the names of players
+    document.getElementById("submitPlayerNamesButton").addEventListener('click', function() {
+
+
+    });
+
+    // Wait for player names to be submitted
+    function waitForUserInput() {
+        setTimeout(function() {
+
+            // do stuff
+
+            if (!submitted) {
+                console.log('here')
+                waitForUserInput();
+            }
+    
+        }, 300); 
+    }
+
+    waitForUserInput();
+}
+
+
+/*
+ * Animates the fadeOut of all the cards and then restarts the game
+ */
+function restartGame() {
+    fadeOutCards();
+
+    setTimeout(function() {
+        dealCards();
+    }, TRANSITION_DURATION)
+}
+
+
+/*
+ * Sets up the game with the 5 starting planes in a cross 
+ */
+function dealCards() {
+    var loadingGif = document.querySelector(".loading-gif");
+    loadingGif.style.display = "inline";
+    loadingGif.classList.add("animate-fadeIn");
+
     for (var i = 0; i < NUM_PLANES; i++) {
         deck[i] = i;
     }
@@ -180,15 +329,51 @@ function setUpGame() {
 
     // Gets first 5 cards from the deck and draws them
     function playInitialCards(deck) {
-        grid[0][1] = deck.shift();
-        grid[1][0] = deck.shift();
-        grid[1][1] = deck.shift();
-        grid[1][2] = deck.shift();
-        grid[2][1] = deck.shift();
+        grid[0][1] = drawCard();
+        grid[1][0] = drawCard();
+        grid[1][1] = drawCard();
+        grid[1][2] = drawCard();
+        grid[2][1] = drawCard();
 
-        renderBoard();
+        renderBoard(true); 
     }
 
     deck = shuffle(deck);
     playInitialCards(deck);
+}
+
+
+/*
+ * Simulates drawing a card from the planar deck
+ * Handles encountering phenomena
+ */
+function drawCard() {
+
+    var card = deck.shift();
+    console.log(card);
+    if (PHENOMENA.includes(card)) {
+        console.log("HERE");
+
+
+
+    }
+
+    return card;
+
+}
+
+
+/*
+ * Animates the fadeOut of all cards
+ * Usage: Whenever changing the cards that are in play (restart game, planeswalk, etc)
+ */
+function fadeOutCards() {
+    var cells = document.querySelectorAll(".cell");
+    var startBackground = document.querySelector(".startBackground");
+    startBackground.style.display = "inline";
+
+    cells.forEach(function(d) {
+        d.classList.remove("animate-fadeIn");
+        d.classList.add("animate-fadeOut");
+    });
 }
